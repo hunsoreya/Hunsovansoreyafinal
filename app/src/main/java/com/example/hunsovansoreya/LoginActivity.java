87ea6c7e54ed8a0d00054c9a468999923506fed9
hunsovansoreya <hunsovansoreya@gitlab.com>
2020-07-22 16:20:52 +0700

    2 commit

--- app/src/main/java/com/example/hunsovansoreya/LoginActivity.java
+++ app/src/main/java/com/example/hunsovansoreya/LoginActivity.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_login);
 
+    }
+    public void login(View view){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+//
 }
