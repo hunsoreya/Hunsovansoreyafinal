2ad53146c6e49763292e4233e39059b648355570
hunsovansoreya <hunsovansoreya@gitlab.com>
2020-07-22 17:26:21 +0700

    3 commit

--- app/src/main/java/com/example/hunsovansoreya/MainActivity.java
+++ app/src/main/java/com/example/hunsovansoreya/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.hunsovansoreya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeActivity()).commit();
     }
+     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+         @Override
+         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+             Fragment selectedFragment=null;
+             switch (item.getItemId()){
+
+                 case R.id.home:
+                     selectedFragment= new HomeActivity();
+                     break;
+                 case R.id.categories:
+                     selectedFragment= new CategoriesActivity();
+                     break;
+                 case R.id.search:
+                     selectedFragment= new SearchActivity();
+                     break;
+                 case R.id.account:
+                     selectedFragment= new AccountActivity();
+                     break;
+             }
+             getSupportFragmentManager().beginTransaction().replace(R.id.container,selectedFragment).commit();
+             return true;
+         }
+     };
 
 }
---
+++ app/src/main/java/com/example/hunsovansoreya/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.hunsovansoreya;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SearchActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.activity_framesearch,container,false);
+    }
+}
